6229f0a4c3a5413fbce16edb508db36988902fbd
Kamin Airnomad <kamin@theinnerlight.nl>
2014-06-28 23:37:56 +0200

    bugfixes

--- nsis-build-installer-win32.sh
+++ nsis-build-installer-win32.sh
@@ -1,5 +1,7 @@
 rm -rf INSTDIR
 ./configure --prefix=`pwd`/INSTDIR --without-webkit1
-make clean all install
+make clean all
 cd src ; strip fennekin.exe ; cd ..
+make install
+
 makensis fennekin.nsi
